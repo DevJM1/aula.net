b40ae8cce0cc9e5f8b8158f2502a36ffabaefa4e
Julia Maria <devjm1@hotmail.com>
2023-01-26 15:10:30 -0300

    aula.det

---
+++ Revisao/Aluno.cs
@@ -0,0 +1,10 @@
+namespace Revisao
+{
+
+    public struct Aluno
+    {
+public string MyProperty {get; set;}
+public decimal Nota {get; set;}
+        public string? Nome { get; internal set; }
+    }
+}
